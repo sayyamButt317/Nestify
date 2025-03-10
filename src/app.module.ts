2e203d2bbe99bd40538ae3df6211838ab5116e34
Sayyam Butt <sayyamButt317@gmail.com>
2025-03-10 22:50:20 +0500

    add modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EpisodesModule } from './episodes/episodes.module';
+import { TopicsModule } from './topics/topics.module';
+import { ConfigsModule } from './configs/configs.module';
 
 @Module({
-  imports: [],
+  imports: [EpisodesModule, TopicsModule, ConfigsModule],
   controllers: [AppController],
   providers: [AppService],
 })
